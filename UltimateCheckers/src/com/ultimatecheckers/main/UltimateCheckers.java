da1c49bea503d9105ae50d5b83b86d83d6209509
Creeoer <scienceaworld@gmail.com>
2022-05-02 19:54:24 -0400

    Created board with checker pieces

--- UltimateCheckers/src/com/ultimatecheckers/main/UltimateCheckers.java
+++ UltimateCheckers/src/com/ultimatecheckers/main/UltimateCheckers.java
@@ -7,7 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class UltimateCheckers extends Application{
+public class UltimateCheckers extends Application{o
     public static void main(String[] args) throws Exception {
         launch(args);
     }
@@ -18,15 +18,10 @@ public class UltimateCheckers extends Application{
 		Button button1 = new Button("Button 1");
 		Button button2 = new Button("Button 2");
 		
-        GridPane gridPane = new GridPane();
-		gridPane.setHgap(10);
-		gridPane.setVgap(10);
-		
-		gridPane.add(button1, 0, 0);
-		gridPane.add(button2, 1, 0);
+        Board checkerBoard = new Board(7);
 
 		
-		Scene scene = new Scene(gridPane, 300, 200);
+		Scene scene = new Scene(checkerBoard.getBoard(), 300, 200);
 		
 		primaryStage.setScene(scene);
 		
