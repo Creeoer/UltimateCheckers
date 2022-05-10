9d6e2d5ca5f0a97f35daa05e4228cf2f92118edb
Creeoer <scienceaworld@gmail.com>
2022-05-09 20:07:25 -0400

    Created checker class

--- UltimateCheckers/src/com/ultimatecheckers/checkers/Checker.java
+++ UltimateCheckers/src/com/ultimatecheckers/checkers/Checker.java
@@ -1,8 +1,74 @@
 package com.ultimatecheckers.checkers;
 
+import javafx.event.EventTarget;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public abstract class Checker {
+
     private Color color;
+    //row column or column row?
+    private int row, column;
+    private Circle checkerPiece;
+
+    private double anchorX, anchorY;
+
+    public Checker(Color color, int row, int column){
+        this.color = color;
+        this.row = row;
+        this.column = column;
+        checkerPiece = new Circle(15);
+        checkerPiece.setFill(color);
+
+        initDraggableHandler();
+    }
     
+
+    private void initDraggableHandler(){
+        //Handle dragging
+
+     checkerPiece.setOnMousePressed(event -> {
+            anchorX = event.getSceneX();
+            anchorY = event.getSceneY();
+            //This is done so that when the mouse is released, it sees the tile under the checker piece
+        //    checkerPiece.setMouseTransparent(true);
+        }
+        );
+
+        checkerPiece.setOnMouseDragged(event -> {
+            checkerPiece.setTranslateX(event.getSceneX() - anchorX);
+            checkerPiece.setTranslateY(event.getSceneY() - anchorY);
+        
+            anchorX = checkerPiece.getTranslateX();
+            anchorY = checkerPiece.getTranslateY();
+        //    checkerPiece.startFullDrag();
+        });
+
+      
+        /*
+         checkerPiece.set(event -> {
+             StackPane tile = ((StackPane) event.getTarget());
+             checkerPiece.setTranslateX(tile.getTranslateX());
+             checkerPiece.setTranslateY(tile.getTranslateY());
+             
+             //Update anchors
+           //  anchorX = tile.getTranslateX();
+          //   anchorY = tile.getTranslateY();
+             checkerPiece.setMouseTransparent(false);
+         });
+*/
+    }
+
+
+    public void setColorAndFill(Color color){
+        this.color = color;
+        checkerPiece.setFill(color);
+      
+    }
+
+    public Circle getElement(){
+        return checkerPiece;
+    }
+
 }
---
+++ UltimateCheckers/src/com/ultimatecheckers/checkers/Pawn.java
@@ -0,0 +1,16 @@
+package com.ultimatecheckers.checkers;
+
+import javafx.scene.paint.Color;
+
+public class Pawn extends Checker{
+    /* This is a normal checker piece that has the standard diagonal movement. Can only go forwards
+    */
+
+    public Pawn(Color color, int row, int column){
+        super(color, row, column);
+    }
+
+
+
+
+}
